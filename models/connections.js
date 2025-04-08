cc1ecd8c0fb9dd4066f4631e85802de3c475740d
ThisIsLuckyluck <denislucas1a@gmail.com>
2025-04-08 16:31:38 +0200

    removing db request

--- models/connections.js
+++ models/connections.js
@@ -11,41 +11,4 @@ const pool = mysql.createPool({
   queueLimit: 0,
 });
 
-async function initDatabase() {
-  try {
-    const createUserTableQuery = `
-      CREATE TABLE IF NOT EXISTS users (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(255) NOT NULL,
-        email VARCHAR(255) NOT NULL,
-        password VARCHAR(255) NOT NULL,
-        token VARCHAR(512) NOT NULL,
-        role INT NOT NULL
-      )
-    `;
-    await pool.query(createUserTableQuery);
-    console.log("Table users vérifiée/créée avec succès !");
-
-    const createProductTableQuery = `
-      CREATE TABLE IF NOT EXISTS product (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(255) NOT NULL,
-        description TEXT NOT NULL,
-        price DECIMAL(8, 2) NOT NULL,
-        type ENUM('appetizer', 'plate', 'dessert') NOT NULL,
-        stock INT DEFAULT 0,
-        image_url VARCHAR(255),
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-      )
-    `;
-    await pool.query(createProductTableQuery);
-    console.log("Table product vérifiée/créée avec succès !");
-  } catch (err) {
-    console.error("Erreur lors de l'initialisation de la base :", err);
-  }
-}
-
-initDatabase();
-
 module.exports = pool;
