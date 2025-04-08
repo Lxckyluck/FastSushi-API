7af273980f2bf0236035ff78b949eb6f642a70ed
ThisIsLuckyluck <denislucas1a@gmail.com>
2025-04-08 16:59:46 +0200

    convert to async request

--- models/product.js
+++ models/product.js
@@ -2,56 +2,56 @@ const db = require("./connections");
 
 const Product = {
   // SQL QUERY to get all products using promise
-  getAll: () => {
-    return db
-      .query("SELECT * FROM product")
-      .then(([results]) => results)
-      .catch((err) => {
-        console.error("Error fetching all products:", err);
-        throw new Error("Error fetching all products");
-      });
+  getAll: async () => {
+    try {
+      const [results] = await db.query("SELECT * FROM product");
+      return results;
+    } catch (err) {
+      console.error("Error fetching all products:", err);
+      throw new Error("Error fetching all products");
+    }
   },
 
   // SQL QUERY to get a product by its ID using promise
-  getById: (id) => {
-    return db
-      .query("SELECT * FROM product WHERE id = ?", [id])
-      .then(([results]) => {
-        if (results.length === 0) {
-          throw new Error("Product not found");
-        }
-        return results[0];
-      })
-      .catch((err) => {
-        console.error(`Error fetching product with ID ${id}:`, err);
-        throw new Error("Error fetching product");
-      });
+  getById: async (id) => {
+    try {
+      const [results] = await db.query("SELECT * FROM product WHERE id = ?", [
+        id,
+      ]);
+      if (results.length === 0) {
+        throw new Error("Product not found");
+      }
+      return results[0];
+    } catch (err) {
+      console.error(`Error fetching product with ID ${id}:`, err);
+      throw new Error("Error fetching product");
+    }
   },
 
   // SQL QUERY to create a new product using promise
-  createProduct: (productData) => {
+  createProduct: async (productData) => {
     const { name, description, price, type, stock, image_url } = productData;
-    return db
-      .query(
+    try {
+      const [result] = await db.query(
         "INSERT INTO product (name, description, price, type, stock, image_url) VALUES (?, ?, ?, ?, ?, ?)",
         [name, description, price, type, stock, image_url]
-      )
-      .then(([result]) => result.insertId)
-      .catch((err) => {
-        console.error("Error creating product:", err);
-        throw new Error("Error while creating the product");
-      });
+      );
+      return result.insertId;
+    } catch (err) {
+      console.error("Error creating product:", err);
+      throw new Error("Error while creating the product");
+    }
   },
 
   // SQL QUERY to delete a product by its ID using promise
-  deleteProductById: (id) => {
-    return db
-      .query("DELETE FROM product WHERE id = ?", [id])
-      .then(([result]) => result)
-      .catch((err) => {
-        console.error(`Error deleting product with ID ${id}:`, err);
-        throw new Error("Error deleting the product");
-      });
+  deleteProductById: async (id) => {
+    try {
+      const [result] = await db.query("DELETE FROM product WHERE id = ?", [id]);
+      return result;
+    } catch (err) {
+      console.error(`Error deleting product with ID ${id}:`, err);
+      throw new Error("Error deleting the product");
+    }
   },
 };
 
