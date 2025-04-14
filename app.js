0692b2baf25a8fd1c8b336e8054999d083993e21
ThisIsLuckyluck <denislucas1a@gmail.com>
2025-04-14 15:56:16 +0200

    adding routes in the app files

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ require("dotenv").config();
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const productsRouter = require("./routes/product");
+const appetizerRouter = require("./routes/appetizer")
+const plateRouter = require("./routes/plate")
+const dessertRouter = require("./routes/dessert")
 
 const app = express();
 const cors = require("cors");
@@ -22,6 +25,9 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/product", productsRouter);
+app.use("/appetizer", appetizerRouter);
+app.use("/plate", plateRouter);
+app.use("/dessert", dessertRouter);
 
 app.listen(port, () => {
   console.log(`Serveur lancé sur http://localhost:${port}`);
