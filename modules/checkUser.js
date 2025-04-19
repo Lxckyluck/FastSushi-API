090ba43f5410d041bdc7009ce08fab5196b6823b
ThisIsLuckyluck <denislucas1a@gmail.com>
2025-04-19 19:55:00 +0200

    testing api restriction

--- modules/checkUser.js
+++ modules/checkUser.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 const authenticateToken = (req, res, next) => {
   const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(" ")[1];
+  const token = localStorage.getItem("authToken")
   if (!token) return res.sendStatus(401);
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
